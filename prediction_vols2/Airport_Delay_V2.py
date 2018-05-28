01b30278b055518ff5481af07f6cde0b3b704b5d
jeanphilippeverhaeghe <jeanphilippe.verhaeghe@gmail.com>
2018-05-28 16:57:10 +0200

    commit 4.0

--- prediction_vols2/Airport_Delay_V2.py
+++ prediction_vols2/Airport_Delay_V2.py
@@ -28,13 +28,13 @@ jour_semaine_dict = { 1:'Lundi',2:'Mardi',3:'Mercredi',4:'Jeudi',5:'Vendredi',6:
 
 # Fonction créant une liste de valeurs distinctes d'une colonne d"un dataframe
 def liste_distincte_col(df, nom_col, sep=""):
-    """Créet une liste de valeurs distinctes d'une colonne d'un dataframe
+    """Créet une liste de valeurs distinctes d'une colonne d"un dataframe
 
     - Args:
         df(pandas.dataframe): dataframe
         nom_col(str): nom de la colonne
         sep(str): chaine de caractère séparateur (optionnel)
-
+        
     - Returns:
         Liste contenant les colonnes contenant la chaîne de caractères.
     """
@@ -61,7 +61,7 @@ def col_rech_titre(df, fin = True, suffix =""):
         df(pandas.dataframe): dataframe
         fin(boolean): flag pour indiquer un préfixe ou un suffixe
         suffix(str): chaîne de caractères recherchée
-
+        
     - Returns:
         Liste contenant les colonnes contenant la chaîne de caractères.
     """
@@ -74,8 +74,8 @@ def col_rech_titre(df, fin = True, suffix =""):
         else:
             for col in df.columns:
                 if col.startswith(suffix) == True:
-                    liste_col.append(col)
-    return liste_col
+                    liste_col.append(col)                  
+    return liste_col 
 
 def MaxFlightsCode(df, airport_code):
     """Comptabilise le nombre de vols sur un code aéroport dans la base complète des vols (fly)
@@ -83,7 +83,7 @@ def MaxFlightsCode(df, airport_code):
     - Args:
         df(pandas.dataframe): dataframe des vols
         airport_code(integer): Code aéroport
-
+        
     - Returns:
         Renvoie le nombre de vols sur l'aéroport donné dans la base df
     """
@@ -101,7 +101,7 @@ def AirportCode (df_fly, df_airport, city):
         df_fly(pandas.dataframe): dataframe des vols
         df_airport(pandas.dataframe): dataframe des aéroports
         city(string): Nom de la ville dont on cherche le code de l'aéroport principal
-
+        
     - Returns:
         Renvoie le AIRPORT_ID d'un aéroport principal d'une ville
     """
@@ -120,26 +120,26 @@ def AirportCode (df_fly, df_airport, city):
         return(0)
 
 
-def prediction2(param, coef, intercept, origin = '', destination = '',
+def prediction2(param, estimators, origin = '', destination = '', 
                 carrier = '', month = 0, weekday = 0):
     '''
     This function allows you to input all of your flight information (no leaks!) and
-    the function will return how late your flight will arrive based on the output from the
-    SGD Regressor.
-
-    Inputs:
-
+    the function will return how late your flight will arrive based on the output from the 
+    Random Forest Regressor.
+        
+    Inputs: 
+        
             Origin (enter this as a city, state combo, or include the airport name (such as Bush
                         or Hobby). This will automatically calculate which airport you meant.
-
+                 
             Destination (same as Origin, entered as a string)
-
+                 
             Carrier (which Airline, use a string to represent the name (such as 'American' or 'United')
-
+                 
             Month (the month the flight is scheduled for)
-
+                 
             Weekday (Enter number between 1-7) such as 1:'Lundi',2:'Mardi',3:'Mercredi',4:'Jeudi',5:'Vendredi',6:'Samedi',7:'Dimanche'
-
+                 
             Available Carriers:
             'FL':'AirTran airways',
             'AS':'Alaska airlines',
@@ -158,23 +158,23 @@ def prediction2(param, coef, intercept, origin = '', destination = '',
             'UA':'United airlines',
             'US':'US airways',
             'VX':'Virgin America
-
-    Outputs:
+            
+    Outputs: 
         int: Estimated delay for the arrival (in minutes, can be negative if the flight is expected to arrive early)
         text: Status of the estimation
     '''
-    from sklearn import linear_model
-
+    from sklearn.ensemble import RandomForestRegressor
+    
     col_utiles = ['CARRIER_DELAY','WEATHER_DELAY', 'NAS_DELAY', 'SECURITY_DELAY', 'LATE_AIRCRAFT_DELAY',
                 'MONTH','DAY_OF_MONTH', 'DAY_OF_WEEK','UNIQUE_CARRIER',
                 'ORIGIN_AIRPORT_ID', 'DEST_AIRPORT_ID','CRS_DEP_TIME',
-                'DISTANCE', 'AIR_TIME','CRS_ELAPSED_TIME','ACTUAL_ELAPSED_TIME', 'ARR_DELAY' ]
+                'DISTANCE', 'AIR_TIME','CRS_ELAPSED_TIME','ACTUAL_ELAPSED_TIME', 'ARR_DELAY' ]    
     #Lecture du fichier d'input
-    fly = pd.read_csv('2016_sample_file_Copy2_00.csv', sep=",", encoding='utf_8', low_memory=False,
+    fly = pd.read_csv('./Dataset_Projet_4/2016_sample_file_09.csv', sep=",", encoding='utf_8', low_memory=False,
                       error_bad_lines=False, usecols = col_utiles)
     #Et celui des aéroports
-    airport = pd.read_csv('L_AIRPORT_ID.csv', sep=",", encoding='utf_8', low_memory=False, error_bad_lines=False)
-
+    airport = pd.read_csv('./Dataset_Projet_4/L_AIRPORT_ID.csv', sep=",", encoding='utf_8', low_memory=False, error_bad_lines=False)
+    
     #Label encoding des cies aériennes
     Cie = liste_distincte_col(fly,'UNIQUE_CARRIER','|')
     Cie.sort()
@@ -191,7 +191,7 @@ def prediction2(param, coef, intercept, origin = '', destination = '',
 
     #Elimination des colonnes Unnamed
     fly = fly.drop(col_rech_titre(fly, False, "Unnamed"), axis=1)
-
+    
     #Recherche du code des aéroports
     id_origin=0
     id_destination=0
@@ -199,44 +199,45 @@ def prediction2(param, coef, intercept, origin = '', destination = '',
         id_origin = AirportCode(fly, airport, origin)
     if destination !='':
         id_destination = AirportCode(fly, airport, destination)
-
+    
     #Création d'un marsk / filtre
     def mask(df, key, value):
         return df[df[key] == value]
     pd.DataFrame.mask = mask
-
+    
     #Filtrage de Fly:
     if id_origin != 0:
         fly = fly.mask('ORIGIN_AIRPORT_ID', id_origin)
-
+    
     if id_destination != 0 :
         fly = fly.mask('DEST_AIRPORT_ID', id_destination)
-
+    
     if month != 0 :
         fly = fly.mask('MONTH', month)
-
+    
     if weekday != 0 :
         fly = fly.mask('DAY_OF_WEEK', weekday)
-
+    
     if carrier != '' :
         #Détermination de la CIE à partir de UNIQUE_CARRIER
         carrier_num = CIEdf[CIEdf.UNIQUE_CARRIER == carrier]
         carrier_num = carrier_num['CIE'].values[0]
         fly = fly.mask('CIE', carrier_num)
-
+    
     #Remplacement des NaN
     fly['CARRIER_DELAY'].fillna(0, inplace=True)
     fly['WEATHER_DELAY'].fillna(0, inplace=True)
     fly['NAS_DELAY'].fillna(0, inplace=True)
     fly['SECURITY_DELAY'].fillna(0, inplace=True)
     fly['LATE_AIRCRAFT_DELAY'].fillna(0, inplace=True)
-
+ 
     if fly.shape[0] == 0 :
         return 0 , "Insuffisament de données pour prédire ! Soyez moins précis !"
 
+    
     #Création des moyennes par colonne
     fly_mean= fly.mean()
-
+    
     #Création d'un DataFRame mono observation reprenant la moyenne des infos nécessaires à la régression:
     ar = np.array([[fly_mean['CARRIER_DELAY'],fly_mean['WEATHER_DELAY'],fly_mean['NAS_DELAY'],
                 fly_mean['SECURITY_DELAY'],fly_mean['LATE_AIRCRAFT_DELAY'],fly_mean['DISTANCE'],
@@ -244,13 +245,15 @@ def prediction2(param, coef, intercept, origin = '', destination = '',
     df = pd.DataFrame(ar, index = [1], columns = ['CARRIER_DELAY','WEATHER_DELAY', 'NAS_DELAY',
                 'SECURITY_DELAY', 'LATE_AIRCRAFT_DELAY','DISTANCE', 'AIR_TIME','CRS_ELAPSED_TIME',
                 'ACTUAL_ELAPSED_TIME'])
-
-
-    New_ridge = linear_model.Ridge(alpha=100)
-    New_ridge.set_params=param
-    New_ridge.coef_ = coef
-    New_ridge.intercept_= intercept
-
-    print("Prédiction d'avance/retard: ", int(New_ridge.predict(df)[0]), " minutes " )
-
-    return int(New_ridge.predict(df)[0]) , "sans erreur"
+    
+    
+    New_rfr = RandomForestRegressor()
+    New_rfr.set_params = param
+    New_rfr.estimators_= estimators
+    
+    print(f"Prédiction d'avance/retard: {int(New_rfr.predict(df)[0])} minutes " )
+    
+    return int(New_rfr.predict(df)[0]) , "prédiction sans erreur"
+    
+    
+    
--- prediction_vols2/views.py
+++ prediction_vols2/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
+from sklearn.ensemble import RandomForestRegressor
 from saisie_param.models import Parametres
 
 # Create your views here.
@@ -10,14 +11,12 @@ from saisie_param.models import Parametres
 from .forms import SaisieVolForm2
 
 def saisie_vol2(request):
-    # Construire le formulaire, avec les données postées,
-    # soit vide si l'utilisateur accède pour la première fois
-    # à la page.
+    # Construire le formulaire
     form = SaisieVolForm2(request.POST or None)
     # Nous vérifions que les données envoyées sont valides
-    # Cette méthode renvoie False s'il n'y a pas de données 
+    # Cette méthode renvoie False s'il n'y a pas de données
     # dans le formulaire ou qu'il contient des erreurs.
-    if form.is_valid(): 
+    if form.is_valid():
         # Ici nous pouvons traiter les données du formulaire
         carrier_delay = form.cleaned_data['CARRIER_DELAY']
         weather_delay = form.cleaned_data['WEATHER_DELAY']
@@ -49,7 +48,7 @@ def saisie_vol2(request):
         print ("distance : ",distance)
         print ("air_time : ",air_time)
         print ("crs_elapsed_time : ",crs_elapsed_time)
-        print ("actual_elapsed_time : ",actual_elapsed_time)       
+        print ("actual_elapsed_time : ",actual_elapsed_time)
         print ("Code Compagnie: " ,Compagnie)
         print ("Code Aeroport_dep: ",Aeroport_dep)
         print ("Code Aeroport_arr: ",Aeroport_arr)
@@ -57,45 +56,59 @@ def saisie_vol2(request):
         #print ("Code JourDep: ",JourDep)
         print ("Code Jour_Semaine: ",JourSemaine)
         #print ("Code HeureDep: ",HeureDep)
-        
-        #extraction des parametres de la Base de données
+
+        #extraction des parametres de la Base de données:
+        #Version pour Ridge
         q1= Parametres.objects.filter(id=2).values('param','coef','intercept')
         param = eval((q1.values_list("param")[0])[0])   #il me faut un class 'dict'
-        coef = np.array((q1.values_list("coef")[0])[0]) #il me faut un class 'numpy.ndarray'        
+        coef = np.array((q1.values_list("coef")[0])[0]) #il me faut un class 'numpy.ndarray'
         intercept = q1.values_list("intercept")[0][0] #il me faut un class 'numpy.float64'
         intercept = np.float64(intercept)
-        
+
         #je n'arrive pas à obtenir en lisant la base de données un numpy.ndarray de float ! Je me le code en dur ! ZUT !
         coef = np.float_([1.16964376,1.08054175,0.6773621,1.16804885,1.17066624,0.0037279,0.05143339,-0.70899413,0.6302446])
-        
+
+        #Version pour RFC
+        #q2= Parametres.objects.filter(id=3).values('param')
+        #param = eval((q2.values_list("param")[0])[0])   #il me faut un class 'dict'
         print("param: ", param)
         print (type(param))
-        print("Coef: ", coef)
-        print(type(coef))
+        print("coef: ", coef)
+        print (type(coef))
         print("intercept: ", intercept)
         print(type(intercept))
-        
-        
+
+
         #Prédiction sans matrice ni fit:
         ar = np.array([[carrier_delay,weather_delay,nas_delay,security_delay,late_aircraft_delay,distance,
                         air_time,crs_elapsed_time,actual_elapsed_time]])
         df = pd.DataFrame(ar, index = [1], columns = ['CARRIER_DELAY','WEATHER_DELAY', 'NAS_DELAY',
                         'SECURITY_DELAY','LATE_AIRCRAFT_DELAY','DISTANCE','AIR_TIME','CRS_ELAPSED_TIME',
                         'ACTUAL_ELAPSED_TIME'])
-        #Ridge:
+        #Version Ridge:
         new_ridge= linear_model.Ridge(alpha=100)
         new_ridge.set_params = param
         new_ridge.coef_ = coef
         new_ridge.intercept_= intercept
         nouvelle_prediction = new_ridge.predict(df)
         pred = float(nouvelle_prediction)
-        comment_pred = "Prédiction à base des coef de la régression Ridge, sans matrice ni FIT"
+        comment_pred = "Prédiction à base des coef de la régression, sans matrice ni FIT"
+        
+        #Version RFC:
+        #new_rfc = RandomForestRegressor()
+        #new_rfc.set_params = param
+        #new_rfc.estimators_= intercept
+        #nouvelle_prediction = new_rfc.predict(df)
+        #pred = float(nouvelle_prediction)
+        #comment_pred = "Prédiction à base des coef de la régression Random Forest Regressor, sans matrice ni FIT"
+        
+        
         print("nouvelle_prediction: ", nouvelle_prediction)
 
         form.fields['Prediction'].label="Prédiction de l'avance/retard de votre vol (en min): " + str(round(pred,2))
         form.fields['Comment_Prediction'].label="Commentaire sur la prédiction: " + comment_pred
-        
-        
+
+
 
         ########################
         #Prédiction plus précise
@@ -106,22 +119,26 @@ def saisie_vol2(request):
                 compa = ''
             else:
                 compa = Compagnie[0]
-            
+
             if len(JourSemaine) == 0:
                 JourS = 0
             else:
                 JourS = int(JourSemaine[0])
-        
+
+            #Version Ridge
             from . import Airport_Delay_V1
-            
             pred, comment_pred = Airport_Delay_V1.prediction2(param = param, coef = coef, intercept = intercept, origin = Aeroport_dep,
                 destination = Aeroport_arr, carrier = compa, month = MoisDep, weekday = JourS)
-        
+
+            #Version Random Forest 
+            #from . import Airport_Delay_V2
+            #pred, comment_pred = Airport_Delay_V2.prediction2(param = param, estimators = intercept, origin = Aeroport_dep,
+            #    destination = Aeroport_arr, carrier = compa, month = MoisDep, weekday = JourS)
+
+
             form.fields['Prediction'].label="Prédiction de l'avance/retard de votre vol (en min): " + str(round(pred,2))
             form.fields['Comment_Prediction'].label="Commentaire sur la prédiction (plus précise): " + comment_pred
-        
-    
+
+
     # Quoiqu'il arrive, on affiche la page du formulaire.
     return render(request, 'saisie_vol2.html', locals())
-
-
